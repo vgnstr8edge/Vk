2aaa3e64267282693e51426d14a17796dee87736
Dmitrii Nasedkin <2_tone@mail.ru>
2021-05-03 14:53:50 +0300

    added class PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,34 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(); {
+        int ownerId;
+        String domain;
+        String query;
+        boolean ownersOnly;
+        int count;
+        int offset;
+
+        // some logic
+
+        return null;
+    }
+
+
+    public void delete() {
+        int ownerId;
+        int postId;
+
+        // some logic
+
+        return null;
+    }
+
+}
+
+
+
